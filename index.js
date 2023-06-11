72542ad4f9f42787f2ce874842035a9d510016b5
anock <anockmsosa@gmail.com>
2023-06-11 15:54:53 +0200

    created server

---
+++ index.js
@@ -0,0 +1,9 @@
+const { log } = require('console');
+const http = require('http');
+const server = http.createServer((req,res)=>{
+res.writeHead(200,{'Content-Type': 'plain/html'})
+})
+
+server.listen(3000,()=>{
+    console.log('server')
+})
