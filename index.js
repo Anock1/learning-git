13af7232b9fe27055f4d89ab6f72c9a6281e6406
Anock <63456199+Anock1@users.noreply.github.com>
2023-06-11 15:58:49 +0200

    added socket

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
 res.writeHead(200,{'Content-Type': 'plain/html'})
 })
-
+const socket = socket.io();
 server.listen(3000,()=>{
     console.log('server')
 })
